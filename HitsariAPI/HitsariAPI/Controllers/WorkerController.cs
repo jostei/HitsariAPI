e2435040448d70dc90c0c3fd20423d8424c8c511
jostei <jose.teittinen@gmail.com>
2021-04-16 18:56:56 +0300

    Lisätty työntekijän postaus

--- HitsariAPI/HitsariAPI/Controllers/WorkerController.cs
+++ HitsariAPI/HitsariAPI/Controllers/WorkerController.cs
@@ -15,20 +15,30 @@ namespace HitsariAPI.Controllers
     {
         // /api/workers GET
         [HttpGet]
-        [Route("Workers")]
+        [Route("tolist")]
         public List<Worker> GetWorkers()
         {
             HitsaritContext konteksti = new();
             return konteksti.Workers.ToList();
         }
 
-        // /api/workers/{workerId}
+        // /api/workers/{workerId} GET
         [HttpGet]
         [Route("{id}")]
         public List<Worker> GetWorker(string id)
         {
             HitsaritContext konteksti = new();
             return konteksti.Workers.Where(c => c.WorkerId == id).ToList();
+        }
+
+        // /api/addWorker POST JSON
+        [HttpPost]
+        [Route("addWorker")]
+        public void AddWorker([FromBody] Worker tyomies)
+        {
+            HitsaritContext konteksti = new();
+            konteksti.Workers.Add(tyomies);
+            konteksti.SaveChanges();
         }
     }
 }
