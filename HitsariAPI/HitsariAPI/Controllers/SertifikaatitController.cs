bb017f067fb4277a71242f32c65e30104410f3c1
jostei <jose.teittinen@gmail.com>
2021-04-19 22:23:05 +0300

    Hitsari React aloitettu

---
+++ HitsariAPI/HitsariAPI/Controllers/SertifikaatitController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HitsariAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace HitsariAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SertifikaatitController : ControllerBase
+    {
+        // /api/getSertificates GET
+        [HttpGet]
+        [Route("getSertificates/{id}")]
+        public List<Sertifikaatit> GetCertificates(string id)
+        {
+            HitsaritContext konteksti = new();
+            Worker w = konteksti.
+            return konteksti.Workers.Where(Worke).ToList();
+        }
+    }
+}
