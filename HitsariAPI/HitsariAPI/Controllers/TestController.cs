f1c8da3cd8130155cb5a968f86b964497f437fba
jostei <jose.teittinen@gmail.com>
2021-04-20 19:33:36 +0300

    Lisätty testikontrolleri

---
+++ HitsariAPI/HitsariAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HitsariAPI.Controllers
+{
+    // Yksinkertainen controlleri ilman tietokantayhteyksiä testaukseen
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public string Testaa()
+        {
+            Console.WriteLine("Testikontrolleri löydetty!");
+            return "Testi OK!";
+        }
+
+    }
+}
