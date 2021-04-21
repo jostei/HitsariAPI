4fe1df02862bfe80ef3b4fe533cca0d009a663fe
jostei <jose.teittinen@gmail.com>
2021-04-21 09:40:35 +0300

    Typon korjaus

--- HitsariAPI/HitsariAPI/Startup.cs
+++ HitsariAPI/HitsariAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace HitsariAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // CORS testiä, poista kuna valmis
+            // CORS testiä, poista kun valmis
             //----------------------------------------------------------------
             services.AddCors(options =>
             {
